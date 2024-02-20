31b09c80219998c2d680cae126f157ce9eeff4c6
Denys Ashikhin <denysashikhin@gmail.com>
2024-02-19 19:29:57 -0500

    updating build files

--- new_build/.next/server/server-reference-manifest.js
+++ new_build/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"2N8Yy1nC9qowf0olChLQ1J0cQa6pyfHGB2li0/Nivus=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"v2tKqFpOj1fqnAM9nI26qhIuofp07of15cjdJeMAsG8=\"}"
